8cadfd8d16ac97c790b2ef20790994b96524d882
ChongyaJin <352577661@qq.com>
2022-09-23 10:19:01 -0700

    commit

--- MyFavoriteFood/MyFavoriteFood/ViewController.swift
+++ MyFavoriteFood/MyFavoriteFood/ViewController.swift
@@ -1,19 +1,27 @@
-//
-//  ViewController.swift
-//  MyFavoriteFood
-//
-//  Created by 醒着像睡着 on 9/23/22.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
+    @IBOutlet weak var tblView: UITableView!
+    let contacts = ["peach","orange","noodle","hamburger","fries"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tblView.delegate = self
+        tblView.dataSource = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = contacts[indexPath.row]
+        return cell
     }
 
-
 }
-
